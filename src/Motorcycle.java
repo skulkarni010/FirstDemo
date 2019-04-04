2a8dff599734f3c822c32a457bcdfc1ea1a75162
skulkarni010 <kulkarnisantosh010.com>
2019-04-04 22:06:51 +0530

    first

---
+++ src/Motorcycle.java
@@ -0,0 +1,24 @@
+class Motorcycle 
+{
+String make;
+String color;
+boolean engineState;
+
+public static void main(String []args) 
+{
+System.out.println("Hello World"); 
+}	
+void startEngine() {
+if (engineState == true)
+{
+System.out.println("The engine is already on.");
+}
+
+else 
+{
+engineState = false;
+System.out.println("The engine is now on.");
+}
+
+}
+}
