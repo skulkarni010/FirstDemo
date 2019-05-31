8e0e4c589e5632938a13fe0fd687626f3c462ee6
root <root@ip-172-31-27-60.us-west-2.compute.internal>
2019-05-31 06:24:26 +0000

    TEST

--- src/Motorcycle.java
+++ src/Motorcycle.java
@@ -6,7 +6,7 @@ boolean engineState;
 
 public static void main(String []args) 
 {
-System.out.println("Hello World"); 
+System.out.println("Hello World1"); 
 System.out.println("Hello World2"); 
 }	
 void startEngine() {
